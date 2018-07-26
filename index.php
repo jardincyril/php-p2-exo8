30cdbef27a96175d9a5ea5669be044bbf99dc6f7
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:38:18 +0200

    condition

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $isOk = 3;
+    if ($isOk) {
+      echo "c\'est ok !!";
+    }
+    else {
+      echo "c\'est pas bon !!!";
+    }
+     ?>
+  </body>
+</html>
